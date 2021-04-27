d277d7d155a4f0e161b2e3277358921dea56e864
Rodolfo <rodolfomolinasv@gmail.com>
2021-04-27 15:16:11 -0600

    Change SpringApplication main class package name to the new one

--- online-store-checkout-api/src/main/java/com/online/store/checkout/api/OnlineStoreCheckoutApiApplication.java
+++ online-store-checkout-api/src/main/java/com/online/store/checkout/api/OnlineStoreCheckoutApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.online.store.checkout.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
